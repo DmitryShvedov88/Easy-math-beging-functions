7b11d3c903c6ecabfd4024776bd7e133d742c569
DmitryShvedov88 <nivka100@gmail.com>
2022-08-26 12:41:56 +0300

    init

---
+++ Matrix/8 Matrix Chess.py
@@ -0,0 +1,12 @@
+
+xy = input()
+y = '87654321'.index(xy[1])
+x = 'abcdefgh'.index(xy[0])
+
+chess=[["." for _ in range(8)] for _ in range(8)]
+chess[y][x]="N"
+for i in range(8):
+    for j in range(8):
+        if (y - i) * (x - j) in [-2, 2]: chess[i][j] = "*"
+for i in range(8):
+    print(*chess[i], sep=" ")
