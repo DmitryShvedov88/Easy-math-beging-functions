512c6229453524963c9542ca5fd0ec58800bed84
DmitryShvedov88 <nivka100@gmail.com>
2022-08-27 15:02:32 +0300

    add verticall variant

--- Matrix/12 Matrix.py
+++ Matrix/12 Matrix.py
@@ -1,5 +1,10 @@
-#Write down the dimensions of the matrix and fill it with numbers in ascending order
+#Write down the dimensions of the matrix and fill it with numbers in ascending order horizontally
 n, m = map(int,input().split())
 Matrix = [[str((1+i)+(j*m)).ljust(3) for i in range (m)] for j in range(n) ]
+for i in range(n):
+    print(*Matrix[i], sep=" ")
+#Write down the dimensions of the matrix and fill it with numbers in ascending order vertically
+n, m = map(int,input().split())
+Matrix = [[str((1+n*i)+j).ljust(3) for i in range (m)] for j in range(n) ]
 for i in range(n):
     print(*Matrix[i], sep=" ")
