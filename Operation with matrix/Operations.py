9eb0d4ee2269a4d1ed8fba9e1add52a30edb72a4
DmitryShvedov88 <nivka100@gmail.com>
2022-09-02 16:08:14 +0300

    add *

--- Operation with matrix/Operations.py
+++ Operation with matrix/Operations.py
@@ -6,6 +6,7 @@ print("Matrix A")
 for i in range(n):
     print(*matrixA[i], sep=" ")
 
+
 print("Введите матрице Б")
 a, s = map(int,input().split())
 matrixB=[[int(input()) for i in range (s)] for j in range(a)]
@@ -20,3 +21,11 @@ for i in range(n):
     print(*matrixC[i], sep=" ")
 
 print("произведение")
+matrixD=[[0 for i in range (m)] for j in range(n)]
+for i in range(n):
+  for j in range(m):
+    for k in range(s):
+       matrixD[i][j] += matrixA[i][k] * matrixB[k][j]
+
+for i in range(n):
+    print(*matrixD[i], sep=" ")
