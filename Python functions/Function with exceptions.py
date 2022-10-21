ef2e6da666e2711e478f0ac38330658ffa9a6108
DmitryShvedov88 <nivka100@gmail.com>
2022-10-21 12:55:18 +0300

    _init_

---
+++ Python functions/Function with exceptions.py
@@ -0,0 +1,9 @@
+def action (a,b):
+    try:
+        result=a/b
+        return result
+    except ZeroDivisionError as ex:
+        print(f'An error accured: {ex}')
+print("Enter the number a and b separated by space")
+a, b= map(int, input().split())
+print(action (a,b))
