90ed7d46c293ae4798d561addb72184d1a98588f
DmitryShvedov88 <nivka100@gmail.com>
2022-10-24 22:28:12 +0300

    #ADD    done

--- Python File I O/Averege student grade.py
+++ Python File I O/Averege student grade.py
@@ -1,5 +1,16 @@
-with open('dataset_3363_2 (9).txt') as t:
-    s = t.readline().strip()
+with open('dataset_3363_4.txt') as t:
+    a=[i.strip().split(";") for i in t]
+for i in a:
+  print((int(i[1])+int(i[2])+int(i[3]))/3)
+mid1=0
+mid2=0
+mid3=0
+lenght=0
 
-a=[input().split(";") for i in range(3)]
-print(a)
+for i in a:
+  lenght+=1
+  mid1+=int(i[1])
+  mid2+=int(i[2])
+  mid3+=int(i[3])
+
+print(mid1/lenght, mid2/lenght, mid3/lenght, sep=" ")
