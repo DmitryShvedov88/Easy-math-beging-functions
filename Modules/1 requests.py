9ecafbf49e6a836a3a708eafb3f8450cc53fc49d
DmitryShvedov88 <nivka100@gmail.com>
2022-10-29 17:32:57 +0300

    add params to requests

--- Modules/1 requests.py
+++ Modules/1 requests.py
@@ -1,3 +1,9 @@
 import requests
 r=requests.get("http://example.com") #get request
 print(r.text)
+
+url="http://example.com"
+par={"key1": "value1", "key2": "value2"}
+r=requests.get(url, params=par) #passing parameters to the request
+print(r.url)#generated url request
+
